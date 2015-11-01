33fc0123c9ba62300aaff948507b5142fd7bac6b
eugene0707 <eugene0707@gmail.com>
2015-11-01 19:42:55 +0300

    Default admin permissions grant admin access to default store

--- lib/spree/permissions.rb
+++ lib/spree/permissions.rb
@@ -36,7 +36,7 @@ module Spree
     end
 
     define_method('default-admin-permissions') do |current_ability, user|
-      current_ability.can :admin, current_store
+      current_ability.can :admin, Spree::Store.default
     end
 
     define_method('can-update-spree/users') do |current_ability, user|
