7f3652dbd6e6429f9cef03eedc4cad528d25bd7d
Priyank Gupta <priyank.gupta@vinsol.com>
2016-09-13 12:11:36 +0530

    add uniquness to roles association in user model

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,6 +1,4 @@
-module Spree
-  Spree.user_class.class_eval do
-    has_many :spree_role_users, class_name: 'Spree::RoleUser'
-    has_many :roles, through: :spree_role_users, class_name: 'Spree::Role'
-  end
+Spree.user_class.class_eval do
+  has_many :spree_role_users, class_name: 'Spree::RoleUser', foreign_key: :user_id
+  has_many :roles, -> { uniq }, through: :spree_role_users, class_name: 'Spree::Role', source: :role
 end
