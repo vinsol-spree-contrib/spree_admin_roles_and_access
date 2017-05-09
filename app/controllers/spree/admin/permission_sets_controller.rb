9704a544ade7d57e24d2b58e52f650c222567fcc
Nimish <nimish@vinsol.com>
2017-05-09 18:45:30 +0530

    Make permission set list sortable

--- app/controllers/spree/admin/permission_sets_controller.rb
+++ app/controllers/spree/admin/permission_sets_controller.rb
@@ -3,6 +3,11 @@ module Spree
     class PermissionSetsController < ResourceController
       before_action :load_permissions, only: [:edit, :new, :create, :update]
 
+      def index
+        @search = Spree::PermissionSet.ransack(params[:q])
+        @permission_sets = @search.result(distinct: true)
+      end
+
       private def permitted_resource_params
         params.require(:permission_set).permit(:name, :description, :display_permission, permission_ids: [])
       end
