6b5bf8bb25c2c3ff279687cde0a1692a715f8ad6
Nimish <nimish@vinsol.com>
2017-05-10 22:41:41 +0530

    Make a widget for quickly showing all available permissions

--- app/assets/javascripts/spree/backend/spree_admin_roles_and_access.js
+++ app/assets/javascripts/spree/backend/spree_admin_roles_and_access.js
@@ -2,25 +2,53 @@
 
 var SearchableList = (function() {
   var SearchableCheckboxList = function(container) {
-    this.$searchBox = $("<input type='text' placeholder='Search..' class='narrow-down-list'></input>");
+    this.$searchBox = $("<div class='input-group input-group-lg col-xs-12'>\
+                           <input type='text' placeholder='Search..' class='narrow-down-list form-control'></input>\
+                         </div>");
     this.$container = container;
     container.before(this.$searchBox);
     this.bindEvents();
   };
 
   SearchableCheckboxList.prototype.bindEvents = function() {
+    this.bindSearch();
+    this.bindCheck();
+    this.formChange();
+  };
+
+  SearchableCheckboxList.prototype.bindCheck = function() {
+    this.$container.find('input:checkbox').on('change', function() {
+      var checkbox = $(this);
+      checkbox.parents('.list-group-item').toggleClass('list-group-item-success');
+      var total = checkbox.parents('.list-group').find('.list-group-item').length;
+      var totalChecked = checkbox.parents('.list-group').find('input:checked').length;
+      checkbox.parents('.panel').find('.count').text(totalChecked + '/' + total);
+    });
+  };
+
+  SearchableCheckboxList.prototype.bindSearch = function() {
     var that = this;
     this.$searchBox.on('keyup', function() {
-      var value = $(this).val();
+      var value = $(this).find('input').val();
       var pattern = new RegExp(value, "i");
 
-      that.$container.find('label').each(function() {
+      that.$container.find('.list-group-item').each(function() {
         if (!($(this).text().search(pattern) >= 0)) {
           $(this).hide();
         } else {
           $(this).show();
         }
       });
+    });
+  };
+
+  SearchableCheckboxList.prototype.formChange = function() {
+    var that = this;
+    var form = this.$container.closest('form');
+    var buttons = form.find('button');
+    buttons.attr('disabled', true);
+    form.find('input').one('change', function() {
+      buttons.attr('disabled', false);
     });
   };
 
